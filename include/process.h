213794520763ffc361b9895c40180a4282b06eca
gerygeo <gergana.georgieva@outlook.com>
2020-07-25 10:33:01 +0000

    fixes after submission

--- include/process.h
+++ include/process.h
@@ -8,7 +8,7 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-    Process (int p, std::string u, std::string com, std::string r, long int ut, float c) : pid(p), user(u), command(com), ram(r), upTime(ut), cpu(c) {}
+    Process (int p, std::string u, std::string com, float c, std::string r, long int ut) : pid(p), user(u), command(com), cpu(c), ram(r), upTime(ut) {}
     int Pid();                               // TODO: See src/process.cpp
     std::string User();                      // TODO: See src/process.cpp
     std::string Command();                   // TODO: See src/process.cpp
